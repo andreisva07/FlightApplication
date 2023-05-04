62dc10f120aed4ae25a91d944bdb6a8570eb6fe4
Ancutaeric <92862412+Ancutaeric@users.noreply.github.com>
2023-05-05 02:26:29 +0300

    Register / Login feature

--- FLightApplication/FLightApplication/Data/ApplicationDbContext.cs
+++ FLightApplication/FLightApplication/Data/ApplicationDbContext.cs
@@ -15,5 +15,13 @@ namespace FlightManagement.Data
         public DbSet<FlightManagement.DataModel.Passenger> Passenger { get; set; } = default!;
         public DbSet<FlightManagement.DataModel.TIcket> TIcket { get; set; } = default!;
         public DbSet<FlightManagement.DataModel.Booking> Booking { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
     }
 }
---
+++ FLightApplication/FlightManagement.DataAccess/Migrations/20230504220226_ericmigration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlightManagement.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ericmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Seats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Number = table.Column<int>(type: "int", nullable: false),
+                    isAvailable = table.Column<bool>(type: "bit", nullable: false),
+                    FlightId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seats", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Seats_Flight_FlightId",
+                        column: x => x.FlightId,
+                        principalTable: "Flight",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seats_FlightId",
+                table: "Seats",
+                column: "FlightId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Seats");
+        }
+    }
+}
